bd82164a4528c58922225f596506410076b00d8d
Mathew Olsen <mathew.olsen@outlook.com>
2022-12-20 16:41:01 -0700

    cleaned up hotkey libs

--- Bitwarden.AutoType.Desktop/Bitwarden.AutoType.Desktop/Services/HotkeyService.cs
+++ Bitwarden.AutoType.Desktop/Bitwarden.AutoType.Desktop/Services/HotkeyService.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using Bitwarden.AutoType.Desktop.Helpers;
 using Bitwarden.AutoType.Desktop.Windows;
-using MahApps.Metro.Controls;
 
 namespace Bitwarden.AutoType.Desktop.Services;
 
-
-
 public class HotkeyService : WPFBackgroundService
 {
-    // private GlobalKeyboardHook _globalKeyboardHook;
-
     private WindowsHotKey _hotKeyNew;
+
     public HotkeyService()
     {
-        //_globalKeyboardHook = new GlobalKeyboardHook();
-        //_globalKeyboardHook.KeyboardPressed += OnKeyPressed;
-
         _hotKeyNew = new WindowsHotKey(VirtualKeys.A, KeyModifier.Ctrl | KeyModifier.Alt, TakAction);
         var success = _hotKeyNew.RegisterHotKey();
     }
@@ -34,38 +21,8 @@ public class HotkeyService : WPFBackgroundService
         MessageBox.Show("OH YEAH");
     }
 
-
-
-    private void OnKeyPressed(object? sender, GlobalKeyboardHookEventArgs e)
-       {
-        if (e.KeyboardData.VirtualCode != GlobalKeyboardHook.VkSnapshot)
-            return;
-
-
-        // seems, not needed in the life.
-        if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.SysKeyDown &&
-            e.KeyboardData.Flags == GlobalKeyboardHook.LlkhfAltdown &&
-            e.KeyboardData.Flags == GlobalKeyboardHook.VK_CONTROL)
-        {
-            MessageBox.Show("Alt + Print Screen");
-            e.Handled = true;
-        }
-        //else
-
-        //if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyDown)
-        //{
-        //    MessageBox.Show("Print Screen");
-        //    e.Handled = true;
-        //}
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-
-
-
-
-
         await Task.Delay(Timeout.Infinite, stoppingToken).ConfigureAwait(false);
     }
 
