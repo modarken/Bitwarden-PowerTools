61bbd13d310b7801457dd41e268b59b82c60806a
Mathew Olsen <mathew.olsen@outlook.com>
2023-05-07 14:31:34 -0700

    installer working but the install package needs fix

--- Bitwarden.AutoType/Bitwarden.AutoType.Desktop.Setup/Sandbox.Installer.cmd
+++ Bitwarden.AutoType/Bitwarden.AutoType.Desktop.Setup/Sandbox.Installer.cmd
@@ -1,4 +1,7 @@
-:: explorer.exe "c:\users\wdagutilityaccount\desktop\en-us"
-start c:\users\wdagutilityaccount\desktop\en-us
-start appwiz.cpl
 msiexec /i c:\users\wdagutilityaccount\desktop\en-us\Bitwarden.AutoType.Desktop.Setup.msi
+start appwiz.cpl
+::start c:\users\wdagutilityaccount\desktop\en-us
+explorer "C:\Program Files"
+explorer "%APPDATA%\..\local"
+::start explorer "C:\Program Files"
+:: explorer.exe "c:\users\wdagutilityaccount\desktop\en-us"
