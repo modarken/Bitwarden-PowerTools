664f100ced23f01c66db693ccc5c267bd8c4680c
Mathew Olsen <mathew.olsen@outlook.com>
2023-05-07 06:51:33 -0700

    refactored project layout

---
+++ Bitwarden.AutoType/Bitwarden.AutoType.Desktop.Setup/Sandbox.Installer.cmd
@@ -0,0 +1,4 @@
+:: explorer.exe "c:\users\wdagutilityaccount\desktop\en-us"
+start c:\users\wdagutilityaccount\desktop\en-us
+start appwiz.cpl
+msiexec /i c:\users\wdagutilityaccount\desktop\en-us\Bitwarden.AutoType.Desktop.Setup.msi
